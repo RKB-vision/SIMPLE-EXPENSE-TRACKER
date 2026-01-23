8ba24092995855aed32c7f0e95e60a2587d5838e
RKB-vision <rkbsapkotavision@gmail.com>
2026-01-23 21:33:10 +0545

    Added search expenses feature

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ let amountInp = document.querySelector("#amount");
 let addBtn = document.querySelector("#add-expense");
 let totalVal=document.querySelector("#total-val")
 let list=document.querySelector("#expense-list")
+let searchInp=document.querySelector("#search")
+
 
 
 let expenses=JSON.parse(localStorage.getItem("history"))||[];
+// let expenses=[];
 render()
 
 addBtn.addEventListener("click",function(){
@@ -22,10 +25,10 @@ addBtn.addEventListener("click",function(){
     render();
 })
 
-function render(){
+function render(dataToPrint = expenses){
    let clutter="";
    let total=0;
-    expenses.forEach(function(item,index){
+    dataToPrint.forEach(function(item,index){
         clutter+=`
         <li><i>${item.name}</i>  : <span class="amo">$${item.amount}</span>
         <button  data-index="${index}" class="del-btn">Delete</button>
@@ -57,4 +60,20 @@ list.addEventListener("click",function(details){
         render();
     }
 
+})
+
+searchInp.addEventListener("input",function(){
+    if(searchInp.value.trim()===""){
+        render();
+        return;
+    }
+    let search=expenses.filter(function(value){
+        if (value.name.toLowerCase().includes(searchInp.value.toLowerCase())){
+            return true
+        }
+        else{
+            return false
+        }
+    })
+    render(search)
 })
